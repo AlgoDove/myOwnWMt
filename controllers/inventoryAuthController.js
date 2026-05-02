95057d2a38e974d110b50f6a67cbc5ac6e59e9a6
AlgoDove <algodove77@gmail.com>
2026-05-03 01:24:02 +0530

    Fix Railway deployment bcrypt issue using bcryptjs

--- controllers/inventoryAuthController.js
+++ controllers/inventoryAuthController.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const InventoryUser = require('../models/InventoryUser');
 const { validateInventoryUserInput } = require('../utils/inventoryValidation');
